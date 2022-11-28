becddbff8bea05a9a75c994bab9d7445e0f594d0
zenghong <1668118834@qq.com>
2022-11-28 18:20:50 +0800

    <update> 更新四叉树测试

--- Assets/QuadTreeUnityTest.cs
+++ Assets/QuadTreeUnityTest.cs
@@ -5,14 +5,13 @@ using ZeroFrame.QuadTree;
 public class QuadTreeUnityTest : MonoBehaviour
 {
 
-    struct Unit : IPos
+    struct Unit : IBounds
     {
 
         public GameObject go;
 
-        public float PosX => go.transform.position.x;
-
-        public float PosY => go.transform.position.y;
+        public System.Numerics.Vector2 LTPos => new System.Numerics.Vector2(go.transform.position.x, go.transform.position.y);
+        public System.Numerics.Vector2 RBPos => new System.Numerics.Vector2(go.transform.position.x, go.transform.position.y);
 
     }
 
@@ -24,25 +23,16 @@ public class QuadTreeUnityTest : MonoBehaviour
 
     void Start()
     {
-        qTree = new QuadTree<Unit>(quadLen, quadTreeLayer);
+        qTree = new QuadTree<Unit>(quadLen, quadTreeLayer, System.Numerics.Vector2.Zero);
         billboard.transform.localScale = new Vector3(quadLen, quadLen, 1);
         Camera.main.orthographicSize = quadLen / 2f;
     }
 
     int index = 0;
-
-    float deltaTime = 0;
     void Update()
     {
         isRunning = true;
         qTree.Tick();
-        // if (deltaTime > 1f)
-        // {
-        //     qTree.Tick();
-        //     deltaTime = 0;
-        // }
-
-        deltaTime += UnityEngine.Time.deltaTime;
 
         if (Input.GetMouseButtonDown(0) && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, 1000))
         {
@@ -66,106 +56,40 @@ public class QuadTreeUnityTest : MonoBehaviour
         }
 
         DrawQuadTree();
-        DrawUnitQuad();
     }
 
     void DrawQuadTree()
     {
         Gizmos.color = Color.red;
-        var keys = qTree.origin_Dic.Keys;
-        var e = keys.GetEnumerator();
+        var e = qTree.quadDic.GetEnumerator();
         while (e.MoveNext())
         {
-            var key = e.Current;
-            var quadOrigin = qTree.origin_Dic[key];
-            var posX = quadOrigin.posX;
-            var posY = quadOrigin.posY;
-            Vector3 pos = new Vector3(posX, posY, 0);
-            var sideLen = quadOrigin.sideLength;
-            var leftPos = pos + new Vector3(-sideLen, 0, 0);
-            var rightPos = pos + new Vector3(sideLen, 0, 0);
-            var upPos = pos + new Vector3(0, sideLen, 0);
-            var downPos = pos + new Vector3(0, -sideLen, 0);
-            Gizmos.DrawLine(leftPos, rightPos);
-            Gizmos.DrawLine(upPos, downPos);
+            var kvp = e.Current;
+            var key = kvp.Key;
+            var quad = kvp.Value;
+            DrawQuad(quad);
         }
 
-        var halfLen = quadLen / 2f;
-        Gizmos.DrawLine(new Vector3(-halfLen, halfLen), new Vector3(halfLen, halfLen));
-        Gizmos.DrawLine(new Vector3(-halfLen, -halfLen), new Vector3(halfLen, -halfLen));
-        Gizmos.DrawLine(new Vector3(-halfLen, halfLen), new Vector3(-halfLen, -halfLen));
-        Gizmos.DrawLine(new Vector3(halfLen, halfLen), new Vector3(halfLen, -halfLen));
     }
 
-    void DrawUnitQuad()
+    static void DrawQuad(Quad<Unit> quad)
     {
-        var unitDic = qTree.unitDic;
-        var origin_Dic = qTree.origin_Dic;
-        var values = qTree.unitDic.Values;
-        foreach (var list in values)
-        {
-            list.ForEach((unit) =>
-            {
-                var posX = unit.PosX;
-                var posY = unit.PosY;
-                // - Self Quad
-                Gizmos.color = Color.green;
-                if (qTree.TryGetQuadOriginAndQuadIndex(posX, posY, out var quadOrigin, out var quadIndex))
-                {
-                    DrawQuad(quadOrigin, quadIndex);
-                }
-                // - Neighbor Quad
-                Gizmos.color = Color.yellow;
-                var originList = qTree.SearchQuad(posX, posY, searchLayer);
-                originList.ForEach((quad) =>
-                {
-                    DrawQuad(quad.quadOrigin, quad.quadIndex);
-                });
-            });
-        }
-    }
+        var ltp = quad.ltPos;
+        var rbp = quad.rbPos;
+        Vector3 ltPos = new Vector3(ltp.X, ltp.Y, 0);
+        Vector3 rbPos = new Vector3(rbp.X, rbp.Y, 0);
+        var len = rbp.X - ltp.X;
 
-    void DrawQuad(QuadOrigin origin, int quadIndex)
-    {
-        Vector3 ltPos = Vector3.zero;
-        Vector3 rtPos = Vector3.zero;
-        Vector3 lbPos = Vector3.zero;
-        Vector3 rbPos = Vector3.zero;
-        var originPos = new Vector3(origin.posX, origin.posY, 0);
-        var sideLength = origin.sideLength;
-        if (quadIndex == 1)
-        {
-            ltPos = originPos + new Vector3(-sideLength, sideLength);
-            rtPos = originPos + new Vector3(0, sideLength);
-            lbPos = originPos + new Vector3(-sideLength, 0);
-            rbPos = originPos;
-        }
-        else if (quadIndex == 2)
-        {
-            ltPos = originPos + new Vector3(0, sideLength);
-            rtPos = originPos + new Vector3(sideLength, sideLength); ;
-            lbPos = originPos;
-            rbPos = originPos + new Vector3(sideLength, 0);
-        }
-        else if (quadIndex == 3)
-        {
-            ltPos = originPos + new Vector3(-sideLength, 0);
-            rtPos = originPos;
-            lbPos = originPos + new Vector3(-sideLength, -sideLength);
-            rbPos = originPos + new Vector3(0, -sideLength);
-        }
-        else if (quadIndex == 4)
-        {
-            ltPos = originPos;
-            rtPos = originPos + new Vector3(sideLength, 0);
-            lbPos = originPos + new Vector3(0, -sideLength);
-            rbPos = originPos + new Vector3(sideLength, -sideLength);
-        }
+        Vector3 rtPos = ltPos;
+        rtPos.x += len;
+        Vector3 lbPos = rbPos;
+        lbPos.x -= len;
 
+        Gizmos.color = Color.green;
         Gizmos.DrawLine(ltPos, rtPos);
+        Gizmos.DrawLine(lbPos, rbPos);
+        Gizmos.DrawLine(ltPos, lbPos);
         Gizmos.DrawLine(rtPos, rbPos);
-        Gizmos.DrawLine(rbPos, lbPos);
-        Gizmos.DrawLine(lbPos, ltPos);
     }
 
 }
